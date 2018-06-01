eff92bc3a650f1306c728567e683db950d480663
Johnson Li <johnsonli1993@gmail.com>
2018-06-01 11:11:33 +0300

    remove unneeded cuda

--- script/create-services.sh
+++ script/create-services.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ssh workstation-vm "docker run -d -e 'functionality=service' -e 'services=RandomGenerator' -e 'job_id=12345' --network host johnson163/hermes-cuda"
+ssh workstation-vm "docker run -d -e 'functionality=service' -e 'services=RandomGenerator' -e 'job_id=12345' --network host johnson163/hermes"
 ssh pi2 "docker run -d -e 'functionality=service' -e 'services=PrintService' -e 'job_id=12345' --network host johnson163/hermes-arm"
 ssh workstation "docker run -d -e 'functionality=service' -e 'services=EchoService' -e 'job_id=12345' --network host johnson163/hermes-cuda"
